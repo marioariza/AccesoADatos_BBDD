873ad3ab13d84fa0e158dd62a8b275d8581064d9
*Mario <*mariojra6@gmail.com*>
2022-12-16 12:43:42 +0100

    Archivos actualizados

--- 003asc.php
+++ 003asc.php
@@ -17,7 +17,9 @@ if(mysqli_connect_errno()) {
     exit();
 }
 
-$consulta = "SELECT * FROM `champ`";
+$row = $_GET['row'];
+
+$consulta = "SELECT * FROM `champ` ORDER BY $row ASC";
 $listaChamp = mysqli_query($conexion, $consulta);
 
 if ($listaChamp) {
@@ -31,9 +33,15 @@ if ($listaChamp) {
                         <path fill-rule='evenodd' d='M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z'/>
                     </svg>
                     </a>
-                    <a href='003desc.php?row=name'>
+                    <a href='003desc.php?row=name' class='text-decoration-none'>
                     <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-arrow-down' viewBox='0 0 16 16'>
                         <path fill-rule='evenodd' d='M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z'/>
+                    </svg>
+                    </a>
+                    <a href='003campeones.php' class='text-decoration-none'>
+                    <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-arrow-counterclockwise' viewBox='0 0 16 16'>
+                        <path fill-rule='evenodd' d='M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z'/>
+                        <path d='M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z'/>
                     </svg>
                     </a>
                     </th>
